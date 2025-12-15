427e1b31bf3c845a8b39d37f69252e1aa8c37090
Mac16 <gilouthomas@gmail.com>
2025-12-15 18:55:38 +0100

    New text

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -5,97 +5,91 @@ import ToolingIcon from './icons/IconTooling.vue'
 import EcosystemIcon from './icons/IconEcosystem.vue'
 import CommunityIcon from './icons/IconCommunity.vue'
 import SupportIcon from './icons/IconSupport.vue'
+import EquipIcon from './icons/IconEquip.vue'
 
 const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
 </script>
 
 <template>
-  <WelcomeItem>
-    <template #icon>
-      <DocumentationIcon />
-    </template>
-    <template #heading>{{ $gettext('Traces & Vols') }}</template>
+  <div class="welcome-outer">
+    <div class="welcome-inner">
+      <WelcomeItem>
+        <template #icon>
+          <DocumentationIcon />
+        </template>
+        <template #heading>{{ $gettext('Flights & Tracks') }}</template>
+        <ul>
+          <li>{{ $gettext('Accédez à votre carnet de vols') }}<router-link :to="{ name: 'logbook-view' }">{{ $gettext('Logbook') }}</router-link></li>
+          <li>  {{ $gettext('Importez de nouvelles traces') }}<router-link :to="{ name: 'import-gps' }">{{ $gettext('Import GPS tracks') }}</router-link></li>
+          <li>{{ $gettext('Examinez des traces IGC ou GPX') }}<router-link :to="{ name: 'external-track' }">{{ $gettext('Review an external GPS track') }}</router-link></li>
+        </ul>
+      </WelcomeItem>
 
-    {{ $gettext('Gérez et analysez tous vos vols. Accédez à votre carnet de vols, importez de nouvelles traces ou examinez des fichiers externes.') }}
-    <br />
-    {{ $gettext('Bravo pour la traduction') }}
-    
-    <ul>
-      <li><router-link :to="{ name: 'logbook-view' }">{{ $gettext('Logbook') }}</router-link></li>
-      <li><router-link :to="{ name: 'import-gps' }">{{ $gettext('Import GPS tracks') }}</router-link></li>
-      <li><router-link :to="{ name: 'external-track' }">{{ $gettext('Review an external GPS track') }}</router-link></li>
-    </ul>
-  </WelcomeItem>
+      <WelcomeItem>
+        <template #icon>
+          <ToolingIcon />
+        </template>
+        <template #heading>{{ $gettext('Statistics') }}</template>
+        <ul>
+          <li>{{ $gettext('Consultez la synthèse annuelle de vos vols') }}<router-link :to="{ name: 'synthese-annee' }">{{ $gettext('Annual summary') }}</router-link></li>
+          <li>{{ $gettext('Obtenez les statistiques globales du carnet de vols') }}<router-link :to="{ name: 'synthese-globale' }">{{ $gettext('Global summary') }}</router-link></li>
+        </ul>    
+      </WelcomeItem>
 
-  <WelcomeItem>
-    <template #icon>
-      <ToolingIcon />
-    </template>
-    <template #heading>{{ $gettext('Tooling') }}</template>
+      <WelcomeItem>
+        <template #icon>
+          <EcosystemIcon />
+        </template>
+        <template #heading>{{ $gettext('Routing') }}</template>
+        <ul>
+          <li>{{ $gettext('Gérez vos waypoints') }}<router-link :to="{ name: 'waypoints-view' }">{{ $gettext('Waypoints') }}</router-link></li>
+          <li>{{ $gettext('Visualisez et éditez les espaces aériens') }}<router-link :to="{ name: 'airspaces-view' }">{{ $gettext('Airspaces') }}</router-link></li>
+          <li>{{ $gettext('Programmez les itinéraires de cross') }}<router-link :to="{ name: 'xcnav-view' }">{{ $gettext('XC Nav') }}</router-link></li>      
+        </ul>
+      </WelcomeItem>
 
-    {{ $gettext('This project is served and bundled with') }}
-    <a href="https://vite.dev/guide/features.html" target="_blank" rel="noopener">Vite</a>. The
-    {{ $gettext('recommended IDE setup is') }}
-    <a href="https://code.visualstudio.com/" target="_blank" rel="noopener">VSCode</a>
-    +
-    <a href="https://github.com/vuejs/language-tools" target="_blank" rel="noopener"
-      >Vue - Official</a
-    >. If you need to test your components and web pages, check out
-    <a href="https://vitest.dev/" target="_blank" rel="noopener">{{ $gettext('Vitest') }}</a>
-    and
-    <a href="https://www.cypress.io/" target="_blank" rel="noopener">{{ $gettext('Cypress') }}</a>
-    /
-    <a href="https://playwright.dev/" target="_blank" rel="noopener">{{ $gettext('Playwright') }}</a>.
-
-    <br />
-
-    {{ $gettext('More instructions are available in') }}
-    <a href="javascript:void(0)" @click="openReadmeInEditor"><code>README.md</code></a
-    >.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <EcosystemIcon />
-    </template>
-    <template #heading>{{ $gettext('Ecosystem') }}</template>
-
-    {{ $gettext('Get official tools and libraries for your project:') }}
-    <a href="https://pinia.vuejs.org/" target="_blank" rel="noopener">Pinia</a>,
-    <a href="https://router.vuejs.org/" target="_blank" rel="noopener">Vue Router</a>,
-    <a href="https://test-utils.vuejs.org/" target="_blank" rel="noopener">Vue Test Utils</a>, and
-    <a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">Vue Dev Tools</a>. If
-    you need more resources, we suggest paying
-    <a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">Awesome Vue</a>
-    a visit.
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <CommunityIcon />
-    </template>
-    <template #heading>{{ $gettext('Community') }}</template>
-
-    {{ $gettext('Got stuck? Ask your question on') }}
-    <a href="https://chat.vuejs.org" target="_blank" rel="noopener">Vue Land</a>
-    (our official Discord server), or
-    <a href="https://stackoverflow.com/questions/tagged/vue.js" target="_blank" rel="noopener"
-      >{{ $gettext('StackOverflow') }}</a
-    >. {{ $gettext('You should also follow the official') }}
-    <a href="https://bsky.app/profile/vuejs.org" target="_blank" rel="noopener">@vuejs.org</a>
-    {{ $gettext('Bluesky account or the') }}
-    <a href="https://x.com/vuejs" target="_blank" rel="noopener">@vuejs</a>
-    {{ $gettext('X account for latest news in the Vue world.') }}
-  </WelcomeItem>
-
-  <WelcomeItem>
-    <template #icon>
-      <SupportIcon />
-    </template>
-    <template #heading>{{ $gettext('Support Vue') }}</template>
-
-    {{ $gettext('As an independent project, Vue relies on community backing for its sustainability. You can help') }}
-    us by
-    <a href="https://vuejs.org/sponsor/" target="_blank" rel="noopener">{{ $gettext('becoming a sponsor') }}</a>.
-  </WelcomeItem>
+      <WelcomeItem>
+        <template #icon>
+          <CommunityIcon />
+        </template>
+        <template #heading>{{ $gettext('Sites') }}</template>
+        <ul>
+          <li>{{ $gettext('Gérez le fichier des sites du carnet de vol') }}<router-link :to="{ name: 'files-base' }">{{ $gettext('Sites') }}</router-link></li>
+          <li>{{ $gettext('Consultez la base de données Paragliding Earth') }}</li>
+        </ul>
+      </WelcomeItem>
+      <WelcomeItem>
+        <template #icon>
+          <EquipIcon />
+        </template>
+        <template #heading>{{ $gettext('Equipment') }}</template>
+        <ul>
+          <li>{{ $gettext('Gérez votre matériel : achat, révision...') }}<router-link :to="{ name: 'equip-base' }">{{ $gettext('Equipment') }}</router-link></li>
+        </ul>
+      </WelcomeItem>
+      <WelcomeItem>
+        <template #icon>
+          <SupportIcon />
+        </template>
+        <template #heading>{{ $gettext('Settings & Utilities') }}</template>
+        <ul>
+          <li>{{ $gettext('Gérez les préférences de l\'application') }}</li>
+          <li>{{ $gettext('Accédez au support') }}</li>
+        </ul>
+      </WelcomeItem>  
+    </div>
+  </div>
 </template>
+<style scoped>
+.welcome-outer {
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  padding-top: 30px;
+}
+.welcome-inner {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+</style>
